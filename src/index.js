4acfab5975f1aa2eb22b878732d3fbf65c880fc5
comic-dev <66639833+comic-dev@users.noreply.github.com>
2021-05-02 16:56:19 +0200

    fix(synt): remove unnecessary stuff

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ module.exports = {
     subbey: async (options) => {
         if (!options) options = { sub: "memes", nfsw: false, max: 3 };
         let { data: { children } } = await (await get(`https://api.reddit.com/r/${options?.sub ?? "memes"}`)).data;
-        return children.filter((v, index) => {
+        return children.filter(v => {
             return v.data.preview && ((options?.nfsw ? v.data.over_18 === true : !v.data.over_18))
-        }).slice(0, options.max).map((v) => {
+        }).slice(0, options.max).map(v => {
             const { title, ups, downs, upvote_ratio, thumbnail, author, all_awardings, url, permalink, subreddit_name_prefixed, is_original_content, over_18, score, is_video } = v.data;
             return {
                 title,
@@ -15,7 +15,7 @@ module.exports = {
                 ratio: `${upvote_ratio * 100}%`,
                 thumbnail,
                 author,
-                awardings: all_awardings.map((v) => { return { name: v.name, icon: v.icon_url } }),
+                awardings: all_awardings.map(v => { return { name: v.name, icon: v.icon_url } }),
                 link: `https://reddit.com/${permalink}`,
                 subreddit: subreddit_name_prefixed,
                 original: is_original_content,
