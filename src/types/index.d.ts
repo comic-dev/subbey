1f9d0b1013b3fddb517aa60379ca8771806e86d8
comic-dev <66639833+comic-dev@users.noreply.github.com>
2021-05-02 13:03:37 +0200

    fix(types): Fix typings

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,5 +1,5 @@
 import { Subbey } from "./Subbey";
 import { Response } from "./Response";
-declare module "subby" {
-    export var subby: (options: Subbey) => Response;
+declare module "subbey" {
+    export default function subby(options: Subbey): Response;
 }
