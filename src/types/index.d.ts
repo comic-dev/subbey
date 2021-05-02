577d722eee29d6e05d1875d2222995a96f7cbfae
comic-dev <66639833+comic-dev@users.noreply.github.com>
2021-05-02 13:20:29 +0200

    fix(types): change return type

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,5 +1,5 @@
 import { Subbey } from "./Subbey";
 import { Response } from "./Response";
 declare module "subbey" {
-    export default function subbey(options: Subbey): Response;
+    export default function subbey(options: Subbey): Promise<Response>;
 }
