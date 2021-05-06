df743c77c9b67a27de0d8d06115bfac371403667
comic-dev <66639833+comic-dev@users.noreply.github.com>
2021-05-06 19:19:13 +0200

    fix(rndm): shuffle posts

--- src/index.js
+++ src/index.js
@@ -3,11 +3,9 @@ module.exports = {
     subbey: async (options) => {
         if (!options) options = { sub: "memes", nfsw: false, max: 3, top: false };
         let { data: { children } } = await (await get(`https://api.reddit.com/r/${options?.sub ?? "memes"}${options?.top ? "/top" : ""}`)).data;
-        return children.filter(v => {
+        const res = children.filter(v => {
             return v.data.preview && ((options?.nfsw ? v.data.over_18 === true : !v.data.over_18))
-        }).sort((a, _) => {
-            return Math.floor(Math.random() * a)
-        }).slice(0, options.max).map(v => {
+        }).map(v => {
             const { title, ups, downs, upvote_ratio, thumbnail, author, all_awardings, url, permalink, subreddit_name_prefixed, is_original_content, over_18, score, is_video } = v.data;
             return {
                 title,
@@ -26,5 +24,13 @@ module.exports = {
                 video: is_video
             }
         })
+        res.forEach((_, i) => {
+            const j = Math.floor(Math.random() * (i-1));
+            const t = res[i];
+            res[i] = res[j];
+            res[j] = t;
+        })
+        return res.slice(0, options.max)
     }
 }
+module.exports.subbey();
